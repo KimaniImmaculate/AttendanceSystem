8efbf66262bcb4856a080b48ce854d44be25d459
Immaculate Kimani <immakym001@gmail.com>
2025-06-17 19:13:06 +0300

    Login Form

---
+++ database/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <title>LoginPage</title>
+</head>
+<body>
+    <div class="loginform">
+        <div class="inputgroup">
+            <input type="text" id="txtUsername" required>
+            <label for="txtUsername" id="lblUsername">USER NAME</label>
+        </div>
+
+        <div class="inputgroup topmargin">
+            <input type="password" id="txtPassword" required>
+            <label for="txtPassword" id="lblPassword">PASSWORD</label>
+        </div>
+
+        <div class="divcallforaction topmargin">
+            <button class="btnlogin">LOGIN</button>
+        </div>
+    </div>
+
+</body>
+</html>
